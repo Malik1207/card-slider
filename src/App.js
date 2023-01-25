3f4056d54748adb5f7387e5e5ae158c15e6c4b75
Malik1207 <abdmalik1207@gmail.com>
2023-01-25 20:43:14 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Default theme
+import "@splidejs/react-splide/css";
+
+// or other themes
+import "@splidejs/react-splide/css/skyblue";
+import "@splidejs/react-splide/css/sea-green";
+
+// or only core styles
+import "@splidejs/react-splide/css/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-primary">
+      <Splide
+        aria-label="My Favorite Images"
+        data-splide='{
+        "type":"loop",
+        "perPage":3,
+        "gap":"1rem",
+        "breakpoints":{
+          "768":{
+            "perPage":2
+          },
+          "576":{
+            "perPage":1
+          }
+        }
+      }'
+      >
+        <SplideSlide>
+          <div class="card">
+            <img src="https://akcdn.detik.net.id/community/media/visual/2023/01/23/cristiano-ronaldo.jpeg?w=700&q=90" alt="Image 1" />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div class="card">
+            <img src="https://akcdn.detik.net.id/community/media/visual/2023/01/23/cristiano-ronaldo.jpeg?w=700&q=90" alt="Image 1" />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div class="card">
+            <img src="https://akcdn.detik.net.id/community/media/visual/2023/01/23/cristiano-ronaldo.jpeg?w=700&q=90" alt="Image 1" />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </SplideSlide>
+      </Splide>
     </div>
   );
 }
